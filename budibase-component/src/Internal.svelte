b67d4bc7ee242f2a0598335185f47254412696d5
shivTO <shivamtomarfeb@gmail.com>
2023-03-31 13:39:36 +0530

    commiting further changes

--- budibase-component/src/Internal.svelte
+++ budibase-component/src/Internal.svelte
@@ -22,8 +22,7 @@
   let max_y = 10;
   let min_x = 0;
   let min_y = 0;
-  let dr={};
-
+  console.log(scaleobject1)
 	const { styleable } = getContext("sdk")
 	const component = getContext("component")
   
@@ -31,39 +30,37 @@
 	
 	function updateData() {
 
-		if (dataProviderdynamic && dataProviderdynamic.rows) {
+		if (dataProviderdynamic) {
 
   Data2 = dataProviderdynamic.rows.filter(function(xx) {
-  if (!xx['ID']) {
+  if (!xx[xaxisd] || !xx[yaxisd]) {
     return false; // skip
   }
   return true;
 }).map(xx => {
     return {
-     
-	  id:xx.ID
+      x: parseFloat(xx[xaxisd]),
+      y: parseFloat(xx[yaxisd]),
     };
   });
 }
-
 if( Data2.length>0 ){
-	if(!dr[Data2[0]['id']]){
-	dr[Data2[0]['id']]=[]}
-		dr[Data2[0]['id']].push(0);
-	  
+	  max_x=Data2[0]['x'];
+	  min_x=Data2[0]['x'];
+	  max_y=Data2[0]['y'];
+	  min_y=Data2[0]['y'];
 	  for(let i=1;i<Data2.length;i++){
-		if(!dr[Data2[i]['id']]) {
-        dr[Data2[i]['id']] = [];
-    }
-    dr[Data2[i]['id']].push(i);
-		
+		max_x=Math.max(max_x,Data2[i]['x']);
+		min_x=Math.min(min_x,Data2[i]['x']);
+		max_y=Math.max(max_y,Data2[i]['y']);
+		min_y=Math.min(min_y,Data2[i]['y']);
 	  }
 	}
 	  scaleObject={
-		max_x: scaleobject1.max_x,
-		max_y: scaleobject1.max_y,
-		min_x:scaleobject1.min_x,
-		min_y: scaleobject1.min_y,
+		max_x: Math.max(max_x,scaleobject1.max_x),
+		max_y: Math.max(max_y,scaleobject1.max_y),
+		min_x:Math.min(min_x,scaleobject1.min_x),
+		min_y: Math.min(min_y,scaleobject1.min_y),
 		nodx: nodx,
 		nody: nody,
 		xaxis: xaxis,
@@ -72,7 +69,7 @@ if( Data2.length>0 ){
 		yaxisd:yaxisd,
 		tc:tc
 	  };
-	  
+	  console.log(scaleObject)
 	
 }
 	function onTrigger() {
@@ -97,7 +94,7 @@ if( Data2.length>0 ){
   <div class="chart">
 	<h1>{tc}</h1>
   
-	<Axis scale_ob={scaleObject} points1={Data2} points={Data} dr={dr} />
+	<Axis scale_ob={scaleObject} points1={Data2} points={Data} />
   
   </div>
   
@@ -105,6 +102,8 @@ if( Data2.length>0 ){
 	.chart {
 	  width: 100%;
 	  height: 100%;
-	  
+	  min-height: 500px;
+	  max-height: 500px;
+	  margin: 16 16;
 	}
   </style>
